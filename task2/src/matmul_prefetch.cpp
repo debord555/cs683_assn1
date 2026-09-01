0d2779499cdb247e850f40af736d493fd57e74cd
Debasish Bordoloi <debasishbordoloi555@gmail.com>
2026-09-01 23:49:20 +0530

    perf: reorder loops

--- task2/src/matmul_prefetch.cpp
+++ task2/src/matmul_prefetch.cpp
@@ -4,22 +4,19 @@
 
 #include "matmul.h"
 
-#define BLK 64
+#define BLK 16
 
 void matmul_prefetch(const float *A, const float *B, float *C,
-                      int M, int N, int K, int lda, int ldb, int ldc) {
+                     int M, int N, int K, int lda, int ldb, int ldc) {
     // TODO(student): replace this placeholder with your cache-blocked SIMD + prefetch
     // implementation.
     for (int bi = 0; bi < M / BLK; bi++) {
         for (int bj = 0; bj < N / BLK; bj++) {
             for (int i = 0; i < BLK; i++) {
                 for (int j = 0; j < BLK; j++) {
-                    C[(bi * BLK + i) * ldc + bj * BLK + j] = 0.0;
-                }
-            }
-            for (int bk = 0; bk < K / BLK; bk++) {
-                for (int i = 0; i < BLK; i++) {
-                    for (int j = 0; j < BLK; j++) {
+                    C[(bi * BLK + i) * ldc + bj * BLK + j] = 0.0f;
+                    for (int bk = 0; bk < K / BLK; bk++) {
+
                         float acc = 0.0f;
 
                         for (int p = 0; p < BLK; p++) {
