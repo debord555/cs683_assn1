5ed905385e031a31dd0730f42433e616032a0f6c
Rahul <rpaul8795@gmail.com>
2026-09-01 22:38:24 +0530

    Implemented Reordering logic

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -1,10 +1,24 @@
-// conv_reorder.cpp  STAGE 1: LOOP REORDERING
-// Hint: loops from outermost to innermost -> ky, kx, oy, ox.
-
 #include "convolution.h"
 
 void conv_reorder(const float* in, float* out, const float* ker,
                   int H, int W, int K) {
-    // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;
+
+    for (int i = 0; i < H * W; ++i) {
+        out[i] = 0.0f;
+    }
+
+    for (int oy = 0; oy < H; ++oy) {
+        for (int ky = 0; ky < K; ++ky) {
+            for (int kx = 0; kx < K; ++kx) {
+                
+                float k_val = ker[ky * K + kx];
+
+                for (int ox = 0; ox < W; ++ox) {
+                    out[oy * W + ox] += in[(oy + ky) * in_stride + (ox + kx)] * k_val;
+                }
+            }
+        }
+    }
 }
