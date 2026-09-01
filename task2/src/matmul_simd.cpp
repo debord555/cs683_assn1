de13d74284792130025ea0b12620e3b2df92c578
Debasish Bordoloi <debasishbordoloi555@gmail.com>
2026-09-01 20:57:37 +0530

    feat: improve simd method

--- task2/src/matmul_simd.cpp
+++ task2/src/matmul_simd.cpp
@@ -8,25 +8,22 @@ void matmul_simd(const float *A, const float *B, float *C,
     // TODO(student): replace this placeholder with your register-tiled AVX2 implementation.
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
-            float acc = 0.0f;
+            __m256 vecAdd = _mm256_setzero_ps();
             const float *a = A + static_cast<long>(i) * lda;
             const float *b = B + static_cast<long>(j) * ldb;
-            for (int p = 0; p < K; p += 8) {
-                __m256 result = _mm256_mul_ps(_mm256_loadu_ps(a + p), _mm256_loadu_ps(b + p));
+            for (int p = 0; p < K; p += 8) 
+                vecAdd = _mm256_fmadd_ps(_mm256_loadu_ps(a + p), _mm256_loadu_ps(b + p), vecAdd);
 
-                __m128 low = _mm256_castps256_ps128(result);
-                __m128 high = _mm256_extractf128_ps(result, 1);
-                __m128 sum = _mm_add_ps(low, high);
+            __m128 low = _mm256_castps256_ps128(vecAdd);
+            __m128 high = _mm256_extractf128_ps(vecAdd, 1);
+            __m128 sum = _mm_add_ps(low, high);
 
-                __m128 sum2 = _mm_movehl_ps(sum, sum);
-                __m128 sum3 = _mm_add_ps(sum, sum2);
-                __m128 sum4 = _mm_movehdup_ps(sum3);
-                __m128 finalVal = _mm_add_ps(sum3, sum4);
+            __m128 sum2 = _mm_movehl_ps(sum, sum);
+            __m128 sum3 = _mm_add_ps(sum, sum2);
+            __m128 sum4 = _mm_movehdup_ps(sum3);
+            __m128 finalVal = _mm_add_ps(sum3, sum4);
 
-                acc += _mm_cvtss_f32(finalVal);
-            }
-
-            C[static_cast<long>(i) * ldc + j] = acc;
+            C[static_cast<long>(i) * ldc + j] = _mm_cvtss_f32(finalVal);
         }
     }
 }
